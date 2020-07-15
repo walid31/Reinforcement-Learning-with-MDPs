f017781d62c01dacdfd50f532d95abac9f3f9365
Walid31 <ghalemwalid31@gmail.com>
2020-07-15 03:55:19 +0100

    applyed bellman equation

--- mdp.py
+++ mdp.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
+import matplotlib.pyplot as plt 
+'exec(%matplotlib inline)'
 
 # Transition Matrix
 props = np.array([
@@ -19,3 +19,21 @@ R = np.array([
     [0, -1, -1, 0, 1], # R(s_3) => s'
     [0, 0, 0, 0, 0]
 ])
+
+
+v = np.zeros(props.shape[0])
+v_old = v.copy()
+gamma = 0.9
+delta = 1e-5
+delta_t = 1
+dif = 1
+
+# Apply the Bellman equation
+while delta_t > delta:
+    for s in range(len(props)):
+        v[s] = np.sum([props[s][sp] * (R[s][sp] + gamma * v_old[sp])\
+            for sp in range(len(props[s]))
+        ])
+    delta_t = np.sum(np.abs(v - v_old))
+    v_old = v.copy()
+print(v)    
